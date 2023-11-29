d2da4c3763e21c94beb9ebac0308726850ece2e4
chibancas <juanjosefdz2003@gmail.com>
2023-11-29 20:12:02 +0000

    servidor apache base

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba de Apache y PHP</title>
+</head>
+<body>
+
+    <h1>chibancas13</h1>
+    <p>Este es un archivo de prueba para verificar que Apache y PHP estén funcionando correctamente.</p>
+
+    <?php
+    // Mostrar información de PHP
+    phpinfo();
+    ?>
+
+</body>
+</html>
---
+++ web/info/info.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1>INFO</h1>
+        <?php
+        // Mostrar información de PHP
+        phpinfo();
+        ?>
+    </body>
+</html> 
